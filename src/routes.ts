b82a07b494a9a2511e3c506754222a1c80e983f7
William Berbet <wiiliamb@hotmail.com>
2022-08-09 14:36:05 -0300

    install packages

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/teste", (req: Request, res: Response) => {
-    return res.json({ nome: 'Comedoria!'})
+    return res.json({ nome: 'William' })
 })
 
 export { router }; 

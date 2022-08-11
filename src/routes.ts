8b47a2571f5faf4737762dff385481b85d99b8cf
William Berbet <wiiliamb@hotmail.com>
2022-08-11 15:30:37 -0300

    create controller + service

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import{ CreateUserController } from "./controllers/user/CreateUserController"
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-    return res.json({ nome: 'William' })
-})
+router.post("/users", new CreateUserController().handle)
 
 export { router }; 

c7d3b60ec68b4bcc627ccf191427035e545a8c24
William Berbet <wiiliamb@hotmail.com>
2022-08-26 18:18:25 -0300

    criando categorias

--- src/routes.ts
+++ src/routes.ts
@@ -3,13 +3,17 @@ import{ CreateUserController } from "./controllers/user/CreateUserController"
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
+
+// ROTAS USER
 router.post("/users", new CreateUserController().handle)
-
 router.post("/login", new AuthUserController().handle)
-
 router.get("/me", isAuthenticated, new DetailUserController().handle)
+
+// ROTAS CATEGORY
+router.post("/category", isAuthenticated, new CreateCategoryController().handle)
 
 export { router }; 

7e4fb92a1d849bae83b9a33e7099730365b52dbe
William Berbet <wiiliamb@hotmail.com>
2022-08-27 12:15:18 -0300

    criando e listando categorias e produtos

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
 
 const router = Router();
 
@@ -15,5 +18,9 @@ router.get("/me", isAuthenticated, new DetailUserController().handle)
 
 // ROTAS CATEGORY
 router.post("/category", isAuthenticated, new CreateCategoryController().handle)
+router.get("/category", isAuthenticated, new ListCategoryController().handle)
+
+// ROTAS PRODUCT
+router.post("/product", isAuthenticated, new CreateProductController().handle)
 
 export { router }; 

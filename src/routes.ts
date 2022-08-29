7958a151d48e36c815111aac324037f7e759036c
William Berbet <wiiliamb@hotmail.com>
2022-08-29 17:42:44 -0300

    removendo pedidos

--- src/routes.ts
+++ src/routes.ts
@@ -7,10 +7,17 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateUserController } from "./controllers/user/CreateUserController"
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
 import { CreateProductController } from "./controllers/product/CreateProductController";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+
+
 
 
 
@@ -31,5 +38,9 @@ router.get("/category", isAuthenticated, new ListCategoryController().handle)
 // ROTAS PRODUCT
 router.post("/product", isAuthenticated, upload.single("file"), new CreateProductController().handle)
 router.get("/category/product", isAuthenticated, new ListByCategoryController().handle)
+
+// ROTAS ORDER - PEDIDO
+router.post("/order", isAuthenticated, new CreateOrderController().handle)
+router.delete("/order", isAuthenticated, new RemoveOrderController().handle)
 
 export { router }; 

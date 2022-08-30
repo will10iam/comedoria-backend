7d0496cae56beed1e691607303e420c6971804c6
William Berbet <wiiliamb@hotmail.com>
2022-08-30 11:50:47 -0300

    enviando pedido

--- src/routes.ts
+++ src/routes.ts
@@ -5,19 +5,20 @@ import uploadConfig  from "./config/multer"
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 import { CreateUserController } from "./controllers/user/CreateUserController"
-import { AuthUserController } from "./controllers/user/AuthUserController";
+import { AuthUserController }   from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
-import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { ListCategoryController }   from "./controllers/category/ListCategoryController";
 
-import { CreateProductController } from "./controllers/product/CreateProductController";
+import { CreateProductController }  from "./controllers/product/CreateProductController";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
-import { AddItemController } from "./controllers/order/AddItemController";
-import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { AddItemController }     from "./controllers/order/AddItemController";
+import { RemoveItemController }  from "./controllers/order/RemoveItemController";
+import { SendOrderController }   from "./controllers/order/SendOrderController";
 
 
 
@@ -46,5 +47,6 @@ router.post("/order", isAuthenticated, new CreateOrderController().handle)
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle)
 router.post("/order/add", isAuthenticated, new AddItemController().handle)
 router.delete("/order/remove", isAuthenticated, new RemoveItemController().handle)
+router.put("/order/send", isAuthenticated, new SendOrderController().handle)
 
 export { router }; 

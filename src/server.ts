790e6b8d87a042c8990579d9c4e4848c14ee4673
William Berbet <wiiliamb@hotmail.com>
2023-07-06 20:30:48 -0300

    port to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response,  NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes";
 import cors from 'cors';
 import "express-async-errors";
@@ -9,12 +9,14 @@ app.use(express.json());
 
 app.use(cors());
 
+const port = process.env.PORT || 3001;
+
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")))
 
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if(err instanceof Error) {
+    if (err instanceof Error) {
         return res.status(400).json({
             error: err.message
         })
